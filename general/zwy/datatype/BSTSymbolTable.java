336bda8dda91635e6494f5b15a55eebc752452f0
iam91 <zhengwy91@foxmail.com>
2016-06-02 10:16:18 +0800

    finish bst

--- general/zwy/datatype/BSTSymbolTable.java
+++ general/zwy/datatype/BSTSymbolTable.java
@@ -1,5 +1,7 @@
 package zwy.datatype;
 
+import java.util.Iterator;
+
 public class BSTSymbolTable<K extends Comparable<K>, V>
 	extends OrderedSymbolTable<K, V>{
 
@@ -15,7 +17,6 @@ public class BSTSymbolTable<K extends Comparable<K>, V>
 			return;
 		}
 		root =  put(key, val, root);
-		n = root.size;
 	}
 
 	private Node put(K key, V val, Node root){
@@ -100,6 +101,7 @@ public class BSTSymbolTable<K extends Comparable<K>, V>
 				root = rightMinNode;
 			}
 		}
+		root.size = size(root.left) + size(root.right) + 1;
 		return root;
 	}
 
@@ -148,28 +150,140 @@ public class BSTSymbolTable<K extends Comparable<K>, V>
 	}
 
 	public K min(){
-		return null;
+		Node minNode = min(root);
+		if(minNode != null){
+			return minNode.key;
+		}
+		else{
+			return null;
+		}
 	}
 
 	public K max(){
-		return null;
+		Node maxNode = max(root);
+		if(maxNode != null){
+			return maxNode.key;
+		}
+		else{
+			return null;
+		}
 	}
+	
 	public K floor(K key){
-		return null;
+		if(key == null){
+			return null;
+		}
+		Node f = floor(key, root);
+		if(f == null){
+			return null;
+		}
+		else{
+			return f.key;
+		}
 	}
+
+	private Node floor(K key, Node root){
+		if(root == null){
+			return null;
+		}
+		int cmp = key.compareTo(root.key);
+		if(cmp > 0){
+			Node t = floor(key, root.right);
+			if(t == null){
+				return root;
+			}
+			else{
+				return t;
+			}
+		}
+		else if(cmp < 0){
+			return floor(key, root.left);
+		}
+		else{
+			return root;
+		}
+	}
+
 	public K ceiling(K key){
-		return null;
+		if(root == null){
+			return null;
+		}
+		Node c = ceiling(key, root);
+		if(c == null){
+			return null;
+		}
+		else{
+			return c.key;
+		}
+	}
+
+	private Node ceiling(K key, Node root){
+		if(root == null){
+			return null;
+		}
+		int cmp = key.compareTo(root.key);
+		if(cmp > 0){
+			return ceiling(key, root.right);
+		}
+		else if(cmp < 0){
+			Node t = ceiling(key, root.left);
+			if(t == null){
+				return root;
+			}
+			else{
+				return t;
+			}
+		}
+		else{
+			return root;
+		}
 	}
 	
 	public int rank(K key){
-		return 0;
+		if(key == null){
+			return 0;
+		}
+		return rank(key, root);
+	}
+
+	private int rank(K key, Node root){
+		if(root == null){
+			return 0;
+		}
+		int cmp = key.compareTo(root.key);
+		if(cmp > 0){
+			return size(root.left) + 1 + rank(key, root.right);
+		}
+		else if(cmp < 0){
+			return rank(key, root.left);
+		}
+		else{
+			return size(root.left);
+		}
 	}
 	
 	public K select(int k){
-		return null;
+		return select(k, root);
 	}
+
+	private K select(int k, Node root){
+		if(root == null){
+			return null;
+		}
+		int sizeLeft = size(root.left);
+		if(sizeLeft > k){
+			return select(k, root.left);
+		}
+		else if(sizeLeft < k){
+			return select(k, root.right);
+		}
+		else{
+			return root.key;
+		}
+	}
+
 	public void deleteMin(){
-		deleteMin(root);
+		root = deleteMin(root);
 	}
 
 	private Node deleteMin(Node root){
@@ -180,21 +294,122 @@ public class BSTSymbolTable<K extends Comparable<K>, V>
 			return root.right;
 		}
 		else{
-			return deleteMin(root.left);
+			root.left = deleteMin(root.left);
+			root.size = size(root.left) + size(root.right) + 1;
+			return root;
 		}
 	}
 
 	public void deleteMax(){
-		return;
+		root = deleteMax(root);
 	}
+
+	private Node deleteMax(Node root){
+		if(root == null){
+			return null;
+		}
+		else if(root.right == null){
+			return root.left;
+		}
+		else{
+			root.right = deleteMax(root.right);
+			root.size = size(root.left) + size(root.right) + 1;
+			return root;
+		}
+	}
+
 	public int size(K lo, K hi){
-		return 0;
+		if(lo == null || hi == null || hi.compareTo(lo) <= 0){
+			return 0;
+		}
+		int rlo = rank(lo, root);
+		int rhi = rank(hi, root);
+		Node ceiling = ceiling(lo, root);
+		if(ceiling != null && ceiling.key.compareTo(hi) == 0){
+			rhi++;
+		}
+		return rhi - rlo + 1;
 	}
+
+	public int size(){
+		if(root == null){
+			return 0;
+		}
+		return root.size;
+	}
+
 	public Iterable<K> keys(){
-		return null;
+
+		if(root == null){
+			return null;
+		}
+		K[] arr = (K[])new Comparable[root.size];
+		traverse(root, arr, 0, null, null);
+
+		return new Iterable<K>(){
+			public Iterator<K> iterator(){
+				return new Iterator<K>(){
+
+					private int curr = 0;
+
+					public boolean hasNext(){
+						return curr < arr.length;
+					}
+
+					public K next(){
+						return arr[curr++];
+					}
+
+					public void remove(){
+
+					}
+				};
+			}
+		};
 	}
+
 	public Iterable<K> keys(K lo, K hi){
-		return null;
+		if(root == null || lo == null || hi == null
+			|| hi.compareTo(lo) <= 0){
+			return null;
+		}
+		int rangeSize = size(lo, hi);
+		K[] arr = (K[])new Comparable[rangeSize];
+		traverse(root, arr, 0, lo, hi);
+
+		return new Iterable<K>(){
+			public Iterator<K> iterator(){
+				return new Iterator<K>(){
+
+					private int curr = 0;
+
+					public boolean hasNext(){
+						return curr < arr.length;
+					}
+
+					public K next(){
+						return arr[curr++];
+					}
+
+					public void remove(){
+
+					}
+				};
+			}
+		};
+	}
+
+	private int traverse(Node root, K[] arr, int curr, K lo, K hi){
+		if(root == null){
+			return curr;
+		}
+		curr = traverse(root.left, arr, curr, lo, hi);
+		if((lo == null && hi == null) 
+			|| (lo.compareTo(root.key) <= 0 && hi.compareTo(root.key) >= 0)){
+			arr[curr++] = root.key;
+		}
+		curr = traverse(root.right, arr, curr, lo, hi);
+		return curr;
 	}
 
 	private class Node{
--- general/zwy/datatype/Test.java
+++ general/zwy/datatype/Test.java
@@ -7,8 +7,11 @@ public class Test{
 		Integer[] f = {1, 5, 7, 0, 2, 6, 9, -1, -3, 45};
 		BSTSymbolTable<Integer, Integer> t 
 			= new BSTSymbolTable<Integer, Integer>();
+		BinarySearchSymbolTable<Integer, Integer> p
+			= new BinarySearchSymbolTable<Integer, Integer>();
 		for(int i = 0; i < 10; i++){
 			t.put(f[i], f[i] + 1);
+			p.put(f[i], f[i] + 1);
 			System.out.print(f[i] + " ");
 		}
 		System.out.println();
@@ -18,11 +21,39 @@ public class Test{
 		System.out.println();
 		System.out.println(t.size());
 		t.delete(0);
+		p.delete(0);
 		
 		for(int i = 0; i < 10; i++){
 			System.out.print(t.get(f[i]) + " ");
 		}
-
+		System.out.println();
+		System.out.println(t.size());
+		
+		Iterator<Integer> iter = t.keys().iterator();
+		while(iter.hasNext()){
+			Integer tt = iter.next();
+			System.out.print(tt + ":" + t.rank(tt) + ":" + p.rank(tt) + " ");
+		}
+		System.out.println();
+		System.out.println(t.ceiling(0));
+		System.out.println(t.floor(6));
+		System.out.println(t.size(1, 6));
+		System.out.println(p.size(1, 6));
+		
+		iter = t.keys(0, 6).iterator();
+		while(iter.hasNext()){
+			System.out.print(iter.next() + " ");
+		}
+		System.out.println();
+		System.out.println(t.min() + ":" + t.max());
+		t.deleteMin();
+		t.deleteMin();
+		t.deleteMax();
+		iter = t.keys().iterator();
+		while(iter.hasNext()){
+			System.out.print(iter.next() + " ");
+		}
+		System.out.println();
 		/*
 		for(int i = 0; i < 10; i++){
 			System.out.print(t.rank(f[i] + 1) + " ");
