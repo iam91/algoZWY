e63136811d138eb507c4f3e556c5dc233acd05ab
iam91 <zhengwy91@foxmail.com>
2016-07-12 23:30:04 +0800

    update

--- general/zwy/datatype/LinearProbingHashST.java
+++ general/zwy/datatype/LinearProbingHashST.java
@@ -23,15 +23,23 @@ public class LinearProbingHashST<K, V> extends SymbolTable<K, V>{
 	}
 
 	private void resize(int cap){
-
+		LinearProbingHashST<K, V> t = new LinearProbingHashST<K, V>(cap);
+		for(int i = 0; i < m; i++){
+			if(keys[i] != null){
+				t.put(keys[i], vals[i]);
+			}
+		}
+		keys = t.keys;
+		vals = t.vals;
+		m = t.m;
 	}
 
 	public void put(K key, V val){
 		if(key == null || val == null){
 			return;
 		}
-		if(2 * N >= M){
-			resize(2 * M);
+		if(2 * n >= m){
+			resize(2 * m);
 		}
 		int i;
 		for(i = hash(key); keys[i] != null; i = (i + 1) % m){
@@ -56,7 +64,70 @@ public class LinearProbingHashST<K, V> extends SymbolTable<K, V>{
 		}
 		return null;
 	}
-	public abstract void delete(K key);
-	public abstract boolean contains(K key);
-	public abstract Iterable<K> keys();
+
+	public void delete(K key){
+		if(key == null){
+			return;
+		}
+		int i = hash(key);
+		while(!key.equals(keys[i]) && keys[i] != null){
+			i = (i + 1) % m;
+		}
+		if(keys[i] == null){
+			return;
+		}
+		keys[i] = null;
+		vals[i] = null;
+		i = (i + 1) % m;
+		while(keys[i] != null){
+			K keyToRedo = keys[i];
+			V valToRedo = vals[i];
+			keys[i] = null;
+			vals[i] = null;
+			n--;
+			put(keyToRedo, valToRedo);
+			i = (i + 1) % m;
+		}
+		n--;
+		if(n > 0 && n <= m / 8){
+			resize(m / 2);
+		}
+	}
+	
+	public boolean contains(K key){
+		if(key == null){
+			return false;
+		}
+		for(int i = hash(key); keys[i] != null; i = (i + 1) % m){
+			if(keys[i].equals(key)){
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public Iterable<K> keys(){
+		return new Iterable<K>(){
+			public Iterator<K> iterator(){
+				return new Iterator<K>(){
+					
+					private int cnt = 0;
+					private int curr = 0;
+
+					public boolean hasNext(){
+						return (cnt++) < n;
+					}
+
+					public K next(){
+						while(curr < m && keys[curr] == null){
+							curr++;
+						}
+						return keys[curr++];
+					}
+
+					public void remove(){}
+				};
+			}
+		};
+	}
 }
--- general/zwy/datatype/T.java
+++ general/zwy/datatype/T.java
@@ -9,21 +9,25 @@ public class T{
 		SeperateChainingHashST<Integer, Integer> t
 			= new SeperateChainingHashST<Integer, Integer>(5);
 
+		LinearProbingHashST<Integer, Integer> tt
+			= new LinearProbingHashST<Integer, Integer>(4);
+
 		Integer ii = 0;
 		System.out.println(ii.hashCode());
 
 		for(int i = 0; i < f.length; i++){
 			t.put(f[i], f[i] + 1);
+			tt.put(f[i], f[i] + 1);
 			System.out.print(f[i] + " ");
 		}
 		System.out.println();
 
-		Iterator<Integer> iter = t.keys().iterator();
+		Iterator<Integer> iter = tt.keys().iterator();
 		while(iter.hasNext()){
 			System.out.print(iter.next() + " ");
 		}
 		System.out.println();
-		System.out.println(t.size());
+		System.out.println(tt.size());
 		System.out.println("=====================================");
 		//t.p();
 	}
