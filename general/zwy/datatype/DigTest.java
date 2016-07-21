cd177f9fa9d072146b0b0c3763690011c3cc5987
iam91 <zhengwy91@foxmail.com>
2016-07-21 11:15:08 +0800

    update

--- general/zwy/datatype/DigTest.java
+++ general/zwy/datatype/DigTest.java
@@ -4,9 +4,17 @@ public class DigTest{
 	public static void main(String[] args){
 		int from[] = {4, 2, 3, 6, 0, 2, 11, 12, 9, 9, 8, 10, 11, 4, 3, 7, 8, 5, 0, 6, 6, 7};
 		int to[] = {2, 3, 2, 0, 1, 0, 12, 9, 10, 11, 9, 12, 4, 3, 5, 8, 7, 4, 5, 4, 9, 6};
-
-		AdjacencyListDigraph gl = new AdjacencyListDigraph(13, 22, from, to);
+		int f[] = {0, 0, 2, 2, 3, 5, 0, 6, 7, 8, 6, 9, 9, 9, 11, 4};
+		int t[] = {1, 5, 0, 3, 5, 4, 6, 4, 6, 7, 9, 10, 12, 11, 12,3};
+		AdjacencyListDigraph gl = new AdjacencyListDigraph(13, 16, f, t);
 
 		System.out.println(gl.toString());
+		DirectedCycle d = new DirectedCycle(gl);
+		if(d.hasCycle()){
+			for(int i: d.cycle()){
+				System.out.print(i + " ");
+			}
+		}
+		System.out.println();
 	}
 }
---
+++ general/zwy/datatype/DirectedCycle.java
@@ -0,0 +1,58 @@
+package zwy.datatype;
+
+public class DirectedCycle{
+	private boolean[] visited;
+	private boolean[] onStack;
+	private boolean hasCycle;
+	private int[] edgeTo;
+	private Stack<Integer> cycle;
+	private int v;
+
+	public DirectedCycle(Digraph graph){
+		v = graph.V();
+		visited = new boolean[v];
+		onStack = new boolean[v];
+		hasCycle = false;
+		edgeTo = new int[v];
+		cycle = null;
+		for(int i = 0; i < v; i++){
+			if(!visited[i]){
+				dfs(graph, i);
+			}
+		}
+	}
+
+	boolean hasCycle(){
+		return hasCycle;
+	}
+
+	Iterable<Integer> cycle(){
+		return cycle;
+	}
+
+	private void dfs(Digraph graph, int s){
+		visited[s] = true;
+		onStack[s] = true;
+		for(int neighbor: graph.adj(s)){
+			if(hasCycle){
+				return;
+			}
+			if(!visited[neighbor]){
+				edgeTo[neighbor] = s;
+				dfs(graph, neighbor);
+			}
+			else if(onStack[neighbor]){
+				System.out.println();
+				hasCycle = true;
+				cycle = (LinkedListStack<Integer>)new LinkedListStack();
+				for(int i = s; i != neighbor; i = edgeTo[i]){
+					cycle.push(i);
+				}
+				cycle.push(neighbor);
+				cycle.push(s);
+				return;
+			}
+		}
+		onStack[s] = false;
+	}
+}
